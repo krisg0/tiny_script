aa45e07e3104cae6602bb8fb82434d76ef621d36
krisg0 <43598910+krisg0@users.noreply.github.com>
2024-09-02 20:09:49 +0800

    Create console_helper.py

---
+++ console_helper.py
@@ -0,0 +1,13 @@
+###############print string format###########################
+#s-string print value in different radix
+#f"{value:format_spec}"
+print(f"{value:8}") #decimal fixed
+print(f"{value:32x}") #hex, fixed width
+print(f"{value:32b}") #binary, fixed width
+print(f"{value:032x}") #0padding, fixed 32characters wide
+###############bitwise operation###########################
+a|b
+a&b
+a^b
+~a
+###############bitwidth and overflow#######################
